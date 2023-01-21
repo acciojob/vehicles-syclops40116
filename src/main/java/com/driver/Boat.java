d09df4e238d7ff8942971ec88e3feb528bac0023
Faiz <syclops40116@gmail.com>
2023-01-21 14:41:57 +0530

    third solution

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -5,7 +5,7 @@ public class Boat implements WaterVehicle {
     private String name;
     private int capacity;
 
-    Boat(String name, int capacity){
+    public Boat(String name, int capacity){
         this.name = name;
         this.capacity = capacity;
     }
